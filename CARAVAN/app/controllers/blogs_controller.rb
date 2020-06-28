3de4a02f879bd39f5c01ed2c6492c4b0c7bd2956
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-06-28 21:52:21 +0900

    [Finish] 4,アプリケーションを完成させよう-chapter7

--- CARAVAN/app/controllers/blogs_controller.rb
+++ CARAVAN/app/controllers/blogs_controller.rb
@@ -1,11 +1,11 @@
 class BlogsController < ApplicationController
 
-  def show
-  	 @blog = Blog.find(params[:id])
-  end
-
   def index
     @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
@@ -19,6 +19,19 @@ class BlogsController < ApplicationController
   end
 
   def edit
+  	@blog = Blog.find(params[:id])
+  end
+
+  def update
+  	blog = Blog.find(params[:id])
+    blog.update(blog_params)
+    redirect_to blog_path(blog)
+  end
+
+  def destroy
+    blog = Blog.find(params[:id])
+    blog.destroy
+    redirect_to blogs_path
   end
 
   private
