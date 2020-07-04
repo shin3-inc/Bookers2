a2d6c51883b1f47e5337f898407adb109692739c
梅澤晴恵 <harueumezawa@HaruenoMacBook-Air.local>
2020-07-05 00:30:18 +0900

    [Finish] 5,アプリケーションを完成させよう-chapter6

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = '1f3a9632942b9fe7c1f9b8864140f0e252e9ddda9ff146c5685cf1c16c8a9a2f11e0762b8fd7d8e8f4d60c86d0f0706811665334dbecb7d08cc83cb37ece24ab'
